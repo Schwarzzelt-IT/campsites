a815094868cc87336d49f4964b5c3678c1fb9d32
2thewes <48988711+2thewes@users.noreply.github.com>
2025-01-18 16:34:20 +0100

    Improved layout and add logo

--- build_webpage/map.js
+++ build_webpage/map.js
@@ -22,6 +22,7 @@ var CampsiteHouseMarker = L.ExtraMarkers.icon({
 	prefix: 'fas'
 });
 
+
 /*
  * Layer definitions.
  */
@@ -41,9 +42,10 @@ var osmOpenTopoMap = new L.TileLayer(
         minZoom: 3,
         maxZoom: 20,
         maxNativeZoom: 17,
-        attribution: 'Kartendaten: &copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap</a>-Mitwirkende, <a href="http://viewfinderpanoramas.org">SRTM</a> | Kartendarstellung: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+        attribution: 'Kartendaten: &copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap</a>, <a href="http://viewfinderpanoramas.org">SRTM</a> | Kartendarstellung: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
     }
 );
+
 
 /*
  * GeoJSON Layer with only campsites
@@ -63,6 +65,7 @@ var campsites = new L.GeoJSON.AJAX(["geojsonFeatures.json"],{
 		return layer.bindPopup("<b>" + feature.properties.name + '</b></br> <a href="'+ feature.properties.url +'" target="_blank">'+ feature.properties.url + "</a>");
 	}
 })
+
 
 /*
  * GeoJSON Layer with only houses
@@ -96,18 +99,29 @@ var overlayLayers = {
 	"Häuser": houses
 };
 
-/*
- * Configure Map.
- * Center is 51°09'48.1"N 10°26'51.7"E 51.163361, 10.447683
- * Add Attribution
- * Default layer is osm
- */
-const map = L.map('map').setView([51.163361, 10.447683], 5);
-map.attributionControl.addAttribution('Einträge gesammmelt von <a href="https://www.schwarzzelt-it.de">Schwarzzelt IT</a>');
-osm.addTo(map)
 
 /*
- * Add both layer as default
+ * Create and configure Map.
+ * Center is 51°09'48.1"N 10°26'51.7"E (51.163361, 10.447683)
+ * Disable default zoom control
+ */
+const map = L.map('map',{zoomControl: false}).setView([51.163361, 10.447683], 5);
+
+
+/*
+ * Add Attribution
+ */
+map.attributionControl.addAttribution('Einträge: <a href="https://www.schwarzzelt-it.de">Schwarzzelt IT</a>');
+
+
+/*
+ * Set default layer
+ */ 
+osm.addTo(map)
+
+
+/*
+ * Add both overlay layers as default
  */
 map.addLayer(campsites);
 map.addLayer(houses);
@@ -118,22 +132,20 @@ map.addLayer(houses);
  */
 let layercontrol = L.control.layers(baseLayers, overlayLayers).addTo(map);
 
+
 /*
  * A nice scale in the bottom left corner.
  */
-L.control.scale().addTo(map);
+L.control.scale({imperial: false, position: 'bottomleft'}).addTo(map);
+
 
 /*
-* Custom Controls
-*/
+ * Github Control
+ */
 var github = L.control({position: 'bottomright'});
     github.onAdd = function(mymap){
         var div = L.DomUtil.create('div', 'github');
         div.innerHTML = `
-			<!--<a class="fa-stack fa-2x" href="#" style="padding-bottom: 40px;padding-right: 40px;">
-				<i class="fas fa-circle fa-stack-2x"style="color: #fff;"></i>
-				<i class="fas fa-info fa-stack-1x" style="color: #000;"></i>
-			</a>--->
 			<div class="leaflet-control-layers leaflet-bar"" style="">
 				<a class="leaflet-control-layers-toggle" href="https://github.com/Schwarzzelt-IT/campsites" target="_blank" style="background-image: none">
 					<i class="fab fa-github"></i>
@@ -144,6 +156,10 @@ var github = L.control({position: 'bottomright'});
     };
 github.addTo(map);
 
+
+/*
+ * Info Control
+ */
 const info = L.control({ position: "topright" });
 info.onAdd = function(map){
 	var container = L.DomUtil.create('div', 'leaflet-control-info');
@@ -154,10 +170,10 @@ info.onAdd = function(map){
 	var content = L.DomUtil.create('section', 'leaflet-control-info-text', container);
 	content.innerHTML = `
 		<span>
-		<b>Campsite Map by Schwarzzelt IT</b>
-		<p>Fast jede Aktion beginnt mit der Suche nach einer großartigen Unterkunft oder einem coolen Zeltplatz. Diese Karte ist eine Übersicht von Gruppenhäusern und Zeltplätzen für Pfadfinder und soll euch bei der Suche unterstützen.</p>
-		<p>Jeder ist herzlich eingeladen an diesem Projekt mitzuarbeiten und die Karte zu ergänzen. Wenn du ein Haus/einen Zeltplatz ergänzen möchtest oder einen Fehler gefunden hast, melde dich gerne unter <a href="mailto:campsites@schwarzzelt-it.de">campsites@schwarzzelt-it.de</a> bei uns oder eröffne einen Issue bzw. Pull-Request bei <a href="https://github.com/Schwarzzelt-IT/campsites" target="_blank">GitHub</a>.</p>
-		<p>Diese Karte wird bereitgestellt vom ehrenamtlichen Projekt <a href="https://www.schwarzzelt-it.de" target="_blank">Schwarzzelt IT</a>.</p>
+			<b>Campsite Map by Schwarzzelt IT</b>
+			<p>Fast jede Aktion beginnt mit der Suche nach einer großartigen Unterkunft oder einem coolen Zeltplatz. Diese Karte ist eine Übersicht von Gruppenhäusern und Zeltplätzen für Pfadfinder und soll euch bei der Suche unterstützen.</p>
+			<p>Jeder ist herzlich eingeladen an diesem Projekt mitzuarbeiten und die Karte zu ergänzen. Wenn du ein Haus/einen Zeltplatz ergänzen möchtest oder einen Fehler gefunden hast, melde dich gerne unter <a href="mailto:campsites@schwarzzelt-it.de">campsites@schwarzzelt-it.de</a> bei uns oder eröffne einen Issue bzw. Pull-Request bei <a href="https://github.com/Schwarzzelt-IT/campsites" target="_blank">GitHub</a>.</p>
+			<p>Diese Karte wird bereitgestellt vom ehrenamtlichen Projekt <a href="https://www.schwarzzelt-it.de" target="_blank">Schwarzzelt IT</a>.</p>
 		</span>
 		`
 	L.DomEvent.on(
@@ -178,3 +194,47 @@ info.onAdd = function(map){
 	return container;
 };
 info.addTo(map);
+
+
+/*
+ * Zoom Control
+ */
+L.control.zoom({position: 'bottomleft'}).addTo(map);
+
+
+/*
+ * Logo Control
+ */
+const logo = L.control({ position: "topleft" });
+logo.onAdd = function (map) {
+	var div = L.DomUtil.create("div", "logo");
+    div.innerHTML = '<a href="https://www.schwarzzelt-it.de/" target="_blank"><img src=""></a>';
+    return div;
+
+	return container;
+};
+logo.addTo(map)
+
+
+/*
+ * Change margin bottom of bottomleft control when changing windows size or base layer
+ * to avoid collision off attribution and scale control
+ */
+setbottommargin();
+window.addEventListener('resize', setbottommargin);
+
+function setbottommargin() {
+	var window_width = document.documentElement.clientWidth;
+	var control_attribution_width = document.querySelector('.leaflet-control-attribution').clientWidth;
+	var control_attribution_height = document.querySelector('.leaflet-control-attribution').clientHeight;
+	var control_scale_width = document.querySelector('.leaflet-control-scale').clientWidth;
+	if (window_width < (control_attribution_width + 10 + control_scale_width)) {
+		document.querySelector('.leaflet-bottom.leaflet-left').style.marginBottom = control_attribution_height + 4 + "px";
+	} else {
+		document.querySelector('.leaflet-bottom.leaflet-left').style.marginBottom = 0 + "px";
+	}
+}
+ map.on('baselayerchange',  async function(e) {
+	await new Promise(r => setTimeout(r,10));
+	setbottommargin();
+  });
